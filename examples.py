cbe5a1a97cbe6fab44c4f1434efbe17b8eafa641
J Olson <jo@alumni.sfu.ca>
2021-02-24 22:15:31 -0800

    Print output

--- examples.py
+++ examples.py
@@ -4,15 +4,15 @@ import dat
 model = dat.Model("glove.840B.300d.txt", "words.txt")
 
 # Compound words are translated into words found in the model
-model.validate("cul de sac") # cul-de-sac
+print(model.validate("cul de sac")) # cul-de-sac
 
 # Compute the cosine distance between 2 words (0 to 2)
-model.distance("cat", "dog") # 0.1983
-model.distance("cat", "thimble") # 0.8787
+print(model.distance("cat", "dog")) # 0.1983
+print(model.distance("cat", "thimble")) # 0.8787
 
 # Compute the DAT score between 2 words (average cosine distance * 100)
-model.dat(["cat", "dog"], 2) # 19.83
-model.dat(["cat", "thimble"], 2) # 87.87
+print(model.dat(["cat", "dog"], 2)) # 19.83
+print(model.dat(["cat", "thimble"], 2)) # 87.87
 
 # Word examples (Figure 1 in paper)
 low = ["arm", "eyes", "feet", "hand", "head", "leg", "body"]
@@ -20,6 +20,6 @@ average = ["bag", "bee", "burger", "feast", "office", "shoes", "tree"]
 high = ["hippo", "jumper", "machinery", "prickle", "tickets", "tomato", "violin"]
 
 # Compute the DAT score (transformed average cosine distance of first 7 valid words)
-model.dat(low) # 50
-model.dat(average) # 78
-model.dat(high) # 95
+print(model.dat(low)) # 50
+print(model.dat(average)) # 78
+print(model.dat(high)) # 95
